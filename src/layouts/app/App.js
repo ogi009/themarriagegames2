d63cd0a5d4dd4e720eb777329ce50aff0154d35c
ogi009 <70148652+ogi009@users.noreply.github.com>
2020-10-27 10:52:52 +0100

    preprared repo

---
+++ src/layouts/app/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Jedziemy!!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
